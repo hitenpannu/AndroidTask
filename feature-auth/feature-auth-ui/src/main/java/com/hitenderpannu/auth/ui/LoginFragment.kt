f59e005d6f281676d9b16ba43b1d8ede5aff88a3
hpannu <hitender.pannu@agoda.com>
2020-04-12 21:10:22 +0700

    Added Layout for login and signup

--- feature-auth/feature-auth-ui/src/main/java/com/hitenderpannu/auth/ui/LoginFragment.kt
+++ feature-auth/feature-auth-ui/src/main/java/com/hitenderpannu/auth/ui/LoginFragment.kt
@@ -1,31 +1,40 @@
 package com.hitenderpannu.auth.ui
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.hitenderpannu.auth.ui.databinding.FragmentLoginBinding
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
 class LoginFragment : Fragment() {
 
+    private var binding: FragmentLoginBinding? = null
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        binding = FragmentLoginBinding.inflate(LayoutInflater.from(context))
+        return binding!!.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        view.findViewById<Button>(R.id.button_first).setOnClickListener {
+
+        binding?.buttonMoveToSignUp?.setOnClickListener {
             findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
         }
+    }
+
+    override fun onDestroyView() {
+        binding = null
+        super.onDestroyView()
     }
 }
