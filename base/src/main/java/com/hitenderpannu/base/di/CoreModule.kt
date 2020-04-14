fb192468db3b77be528a0989b89f620903636107
hpannu <hitender.pannu@agoda.com>
2020-04-14 19:29:29 +0700

    Added okhttp client builder to add user token as every request header

--- base/src/main/java/com/hitenderpannu/base/di/CoreModule.kt
+++ base/src/main/java/com/hitenderpannu/base/di/CoreModule.kt
@@ -8,6 +8,7 @@ import com.hitenderpannu.common.domain.UserPreferences
 import com.hitenderpannu.common.utils.NetworkConnectionChecker
 import dagger.Module
 import dagger.Provides
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
@@ -17,9 +18,25 @@ class CoreModule(private val application: MainApplication) {
 
     @Singleton
     @Provides
-    fun provideRetrofit(): Retrofit = Retrofit.Builder()
+    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit = Retrofit.Builder()
         .baseUrl(BuildConfig.BASE_URL)
+        .client(okHttpClient)
         .addConverterFactory(GsonConverterFactory.create()).build()
+
+    @Singleton
+    @Provides
+    fun provideOkHttpClient(userPreferences: UserPreferences): OkHttpClient {
+        val builder = OkHttpClient.Builder()
+        builder.addInterceptor { chain ->
+            val originalRequest = chain.request()
+            val modifiedRequest = originalRequest.newBuilder()
+                .header("token", userPreferences.userToken ?: "")
+                .method(originalRequest.method(), originalRequest.body())
+                .build()
+            chain.proceed(modifiedRequest)
+        }
+        return builder.build()
+    }
 
     @Singleton
     @Provides
