fe5ddf26e0a210e0c4eb8e1fcba9173fcc75e43e
hpannu <hitender.pannu@agoda.com>
2020-05-20 17:58:36 +0700

    Added Drag feature

--- feature-task/feature_task_ui/src/main/java/com/hitenderpannu/task/ui/taskFragment/TaskFragment.kt
+++ feature-task/feature_task_ui/src/main/java/com/hitenderpannu/task/ui/taskFragment/TaskFragment.kt
@@ -64,6 +64,18 @@ class TaskFragment : Fragment() {
 
             override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
             }
+
+            override fun onSelectedChanged(viewHolder: RecyclerView.ViewHolder?, actionState: Int) {
+                super.onSelectedChanged(viewHolder, actionState)
+                if (actionState == ItemTouchHelper.ACTION_STATE_DRAG) {
+                    viewHolder?.itemView?.alpha = 0.5f
+                }
+            }
+
+            override fun clearView(recyclerView: RecyclerView, viewHolder: RecyclerView.ViewHolder) {
+                super.clearView(recyclerView, viewHolder)
+                viewHolder.itemView.alpha = 1f
+            }
         }
     }
 
